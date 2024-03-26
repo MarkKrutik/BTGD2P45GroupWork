178f1877728136c65fe940cfc3ea0ad60275cff2
KenanButler <kenan.butler909@gmail.com>
2024-03-26 02:37:12 -0400

    more UI added

--- Celeritas Lux/Assets/Scripts/UI/pausemenu.cs
+++ Celeritas Lux/Assets/Scripts/UI/pausemenu.cs
@@ -2,32 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class pausemenu : MonoBehaviour
 {
     public static bool gamepaused = false;
 
+    public static bool options = false;
+
     public GameObject PauseMenu;
+
+    public GameObject OptionsMenu;
+
     
     void Update()
     {
         
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            
             if (gamepaused)
             {
+                if (!options)
+                {
+                    Resume();
+                }
+                if (options)
+                {
+                    optionsoff();
+                }
                 
-                Resume();
             }
             else
             {
+                
                 Pause();
             }
         }
         
     }
 
-    void Resume()
+    public void Resume()
     {
         PauseMenu.SetActive(false);
         Time.timeScale = 1f;
@@ -39,5 +54,32 @@ public class pausemenu : MonoBehaviour
         PauseMenu.SetActive(true);
         Time.timeScale = 0f;
         gamepaused = true;
+    }
+
+    void Optionson()
+    {   
+        options = true; 
+        OptionsMenu.SetActive(true);
+        PauseMenu.SetActive(false );
+        
+    }
+
+    void optionsoff()
+    {   
+        options = false;
+        OptionsMenu.SetActive(false);
+        PauseMenu.SetActive(true);
+
+    }
+
+    public void openoptions()
+    {
+        Optionson();
+    }
+
+    public void mainmenu()
+    {
+        
+        SceneManager.LoadScene(1);
     }
 }
